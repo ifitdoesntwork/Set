1b4963f2b8d284ad90a0322d4dc5099909db0df3
Denis Avdeev <davdeev@me.com>
2024-04-17 09:36:48 +0500

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Denis Avdeev on 17.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
